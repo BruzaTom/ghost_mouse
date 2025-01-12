cf46cfb6f816b4e19809a87c6c729bfb5a1fa7b4
BruzaTom <thomasagomez77@gmail.com>
2025-01-12 10:56:51 -0600

    created an app class and working on overlay window

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ghost_mouse
 records mouse movements for playback to automate tasks.
+must select window from open windows list and record mouse clicks before pressing play.
--- ghost_mouse.py
+++ ghost_mouse.py
@@ -1,15 +1,12 @@
 import tkinter as tk# import tkinter
-from pynput.mouse import Listener
+from pynput.mouse import Listener, Button
 from pynput import keyboard, mouse
 import pygetwindow as gw
 from pywinauto import Application
-import win32gui
-import win32con
 import time
-import struct
-import win32api
 import autoit
 import ast
+import ctypes
 
 root = tk.Tk() # create a window
 root.title("Ghost Mouse")
@@ -33,130 +30,164 @@ def getLst(file):
     dictLst = ast.literal_eval(data)
     return dictLst
 
-data = getLst(file)
-recorded_actions = data[1]
-window = data[0]
-user_poses = []
-time_stamps = data[2]
-timer = 0.0
-start_time = 0.0
-ctrl_pressed = False
+class terminal:
+    def __init__(self):
+        self.root2 = tk.Tk()
+        self.root2.title("ghost_terminal")
+        self.root2.geometry("300x400+100+100")
+        self.root2.overrideredirect(True) #make borderless and click-through
+        self.root2.attributes("-transparentcolor", "white")
+        self.root2.wm_attributes("-topmost", True)
+        self.root2.attributes("-alpha", 0.7)
+        self.row = 0
+        self.hwnd = ctypes.windll.user32.GetParent(self.root2.winfo_id())
+        exstyle = ctypes.windll.user32.GetWindowLongW(self.hwnd, -20)
+        ctypes.windll.user32.SetWindowLongW(self.hwnd, -20, exstyle | 0x00080000 | 0x00000020)
+
+    def term_lable(self, string):
+        label = tk.Label(self.root2, text=string, font=("Arial", 10), fg = green)
+        label.grid(row=self.row, column=0, padx=10, pady=10)
+        self.row += 1
+
+    def lift(self):
+        self.root2.lift()
+
+class app:
+    def __init__(self, file):
+        self.file = file
+        self.data = getLst(file)
+        self.recorded_actions = self.data[1]
+        print(f"recorded actions: {self.recorded_actions}")
+        self.window = self.data[0]
+        print(f"window: {self.window}")
+        self.user_poses = []
+        self.time_stamps = self.data[2]
+        print(f"time stamps: {self.time_stamps}")
+        self.timer = 0.0
+        self.start_time = 0.0
+        self.ctrl_pressed = False
+        self.stop = False
+        self.mouse_listener = None
+        self.key_listener = None
+        self.key_listener2 = None
+
+    def on_click(self, x, y, button, pressed):
+        if pressed:
+            if button == Button.left:
+                self.recorded_actions.append(('left_click', (x, y)))
+            elif button == Button.right:
+                self.recorded_actions.append(('right_click', (x, y)))
+            self.capture_click_time()
+
+    def capture_click_time(self):
+        click_time = time.perf_counter()
+        delta_time = click_time - self.start_time
+        self.start_time = click_time
+        print(f"Click at {click_time}, Delta Time: {delta_time}")
+        self.time_stamps.append(delta_time)
+
+    def play_back(self, actions, window_title, repetitions):
+        self.focus_window(window_title)
+        #transperant = terminal()
+        self.key_listener2 = keyboard.Listener(on_press=self.on_press2)
+        self.key_listener2.start()
+        for t in range(repetitions):
+            #transperant.lift()
+            if self.stop:
+                forget_all(root)
+                main()
+            print(t)
+            i = 0
+            for action, pos in actions:
+                time.sleep(self.time_stamps[i])
+                #mouse_pos = autoit.mouse_get_pos() # back to user position
+                if self.stop == False:
+                    time.sleep(0.08)
+                    if action == 'left_click':
+                        autoit.mouse_move(pos[0], pos[1], speed=0)
+                        time.sleep(0.08)
+                        autoit.mouse_click('left', pos[0], pos[1])
+                        print(f"left Clicked at {pos}")
+                    if action == 'right_click':
+                        autoit.mouse_move(pos[0], pos[1], speed=0)
+                        time.sleep(0.08)
+                        autoit.mouse_click('right', pos[0], pos[1])
+                        print(f"right Clicked at {pos}")
+                    #autoit.mouse_move(mouse_pos[0], mouse_pos[1], speed=0) # back to user position
+                    i += 1
+                else:
+                    forget_all(root)
+                    main()
+            time.sleep(self.time_stamps[i])
+
+    def play(self):
+        repetitions = 1000
+        self.focus_window(self.window)
+        self.play_back(self.recorded_actions, self.window, repetitions)
+        
+
+    def record(self):
+        #forget_all(root)
+        #makeLable("\n\n\n\nRecording\nPress f to stop.", 16)
+        self.mouse_listener = mouse.Listener(on_click=self.on_click) #need to implement on recording
+        self.key_listener = keyboard.Listener(on_press=self.on_press)
+        self.recorded_actions = []
+        self.time_stamps = []
+        self.start_time = time.perf_counter()
+        self.focus_window(self.window)
+        self.mouse_listener.start()
+        self.key_listener.start()
+
+    def selectWindow(self):#print and select from open windows
+        forget_all(root)
+        windows = gw.getAllTitles()#makes list of window names
+        windows = [win for win in windows if win] #need to research this weird code
+        def getName(name):
+            lst = [name, self.recorded_actions, self.time_stamps]
+            updateFile(lst, file)
+            forget_all(root)
+            main()
+        makeLable(f"\n\nOpen windows.", 18)
+        for window in windows:
+            makeButton(window, lambda w = window: getName(w), hi=1, wi=70)
+
+    def focus_window(self, window_title):
+        window = gw.getWindowsWithTitle(window_title)
+        time.sleep(0.25)
+        window[0].activate()
+
+    def on_press(self, key):
+        if key == keyboard.KeyCode.from_char('f'):
+            self.capture_click_time()
+            self.mouse_listener.stop() # Stop mouse recording
+            self.key_listener.stop()
+            lst = [self.window, self.recorded_actions, self.time_stamps]
+            updateFile(lst, file)
+            print(self.recorded_actions)
+            print(self.time_stamps)
+            self.focus_window("Ghost Mouse")
+
+    def on_press2(self, key):
+        if key == keyboard.KeyCode.from_char('u'):
+            self.stop = True
+            self.key_listener2.stop()
+            print("stopped")
+            self.focus_window("Ghost Mouse")
+
+def main():
+    user = app("ghost_data.txt")
+    print("main function")
+    makeLable(f"\n\nWindow: {user.window}\n", 18)
+    makeButton("Select Window", user.selectWindow, wi=20)
+    makeButton("Record", user.record, wi=20)
+    makeButton("Play", user.play, wi=20)
+    tk.Label(root, text='\n\n\nDeveloped By Thomas Gomez @https://github.com/BruzaTom', fg=lablelc, bg="#333333", font=("Arial", 10, "bold")).pack()
+    root.mainloop()
+    
 
 def updateFile(Lst, file):
     with open(file, "w") as f:
         f.write(str(Lst))
-
-def on_click(x, y, button, pressed):
-    if pressed:
-        recorded_actions.append(('click', (x, y)))
-        capture_click_time()
-
-def capture_click_time():
-    global start_time
-    click_time = time.perf_counter()
-    delta_time = click_time - start_time
-    start_time = click_time
-    print(f"Click at {click_time}, Delta Time: {delta_time}")
-    time_stamps.append(delta_time)
-
-stop = False
-def play_back(actions, window_title, repetitions):
-    focus_window(window_title)
-    key_listener2.start()
-    for t in range(repetitions):
-        if stop:
-            break
-        print(t)
-        i = 0
-        for action, pos in actions:
-            time.sleep(time_stamps[i])
-            mouse_pos = autoit.mouse_get_pos() # back to user position
-            if stop == False:
-                if action == 'click':
-                    autoit.mouse_move(pos[0], pos[1], speed=0)
-                    time.sleep(0.08)
-                    autoit.mouse_click('left', pos[0], pos[1])
-                    print(f"Clicked at {pos}")
-                autoit.mouse_move(mouse_pos[0], mouse_pos[1], speed=0) # back to user position
-                i += 1
-        time.sleep(time_stamps[i])
-    forget_all(root)
-    main()
-
-mouse_listener = mouse.Listener(on_click=on_click)
-
-def record(window):
-    global recorded_actions
-    recorded_actions = []
-    global start_time
-    start_time = time.perf_counter()
-    focus_window(window)
-    mouse_listener.start()
-    key_listener.start()
-    forget_all(root)
-    main()
-
-def selectWindow():#print and select from open windows
-    forget_all(root)
-    windows = gw.getAllTitles()#makes list of window names
-    windows = [win for win in windows if win] #need to research this weird code
-    def getName(name):
-        lst = [name, recorded_actions, time_stamps]
-        updateFile(lst, file)
-        forget_all(root)
-        main()
-    makeLable(f"\n\nOpen windows.", 18)
-    for window in windows:
-        makeButton(window, lambda w = window: getName(w), hi=1, wi=70)
-
-def focus_window(window_title):
-    window = gw.getWindowsWithTitle(window_title)
-    time.sleep(0.25)
-    window[0].activate()
-
-def on_press(key):
-    if key == keyboard.KeyCode.from_char('f'):
-        mouse_listener.stop() # Stop mouse recording
-        capture_click_time()
-        key_listener.stop()
-        lst = [window, recorded_actions, time_stamps]
-        updateFile(lst, file)
-        print(recorded_actions)
-        print(time_stamps)
-        focus_window("Ghost Mouse")
-
-def on_press2(key):
-    global stop
-    if key == keyboard.KeyCode.from_char('u'):
-        stop = True
-        key_listener2.stop()
-        print("stopped")
-        focus_window("Ghost Mouse")
-
-key_listener2 = keyboard.Listener(on_press=on_press2)
-key_listener = keyboard.Listener(on_press=on_press)
-
-def on_release(key):
-    global ctrl_pressed
-    if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
-        ctrl_pressed = False
-
-def main():
-    user_data = getLst(file)
-    print("main function")
-    window = user_data[0]
-    recorded_actions = user_data[1]
-    def play():
-        repetitions = 1000
-        focus_window(window)
-        play_back(recorded_actions, window, repetitions)
-    
-    makeLable(f"\n\nWindow: {window}\n", 18)
-    makeButton("Select Window", selectWindow, wi=20)
-    makeButton("Record", lambda w = window: record(w), wi=20)
-    makeButton("Play", play, wi=20)
-    tk.Label(root, text='\n\n\nDeveloped By Thomas Gomez @https://github.com/BruzaTom', fg=lablelc, bg="#333333", font=("Arial", 10, "bold")).pack()
-    root.mainloop()
 
 #entrys bg color change on focus
 def on_focus_in(event):
